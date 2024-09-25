0caa27847aa24e0600e4a8ca6e0328e1e7379e05
Trong_Minh <trongminhmcw10e@gmail.com>
2024-09-26 01:04:02 +0700

    update image processing

--- test_dinov2.py
+++ test_dinov2.py
@@ -2,6 +2,9 @@ import torch
 from PIL import Image
 from torchvision import datasets, models, transforms
 
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import MinMaxScaler
+
 if torch.cuda.is_available():
     device = torch.device('cuda')
 elif torch.backends.mps.is_available():
@@ -14,17 +17,31 @@ print(f'using {device}')
 dinov2_vits14 = torch.hub.load('facebookresearch/dinov2', 'dinov2_vits14')
 dinov2_vits14 = dinov2_vits14.to(device)
 
-img_path = ''
+pca = PCA(n_components=3)
+scaler = MinMaxScaler(clip=True)
+patch_size = dinov2_vits14.patch_size
 
-IMG_SIZE = 512
+# original image of size 1024x1024
+IMG_SIZE = 1024 
+#520//14
+patch_h  = IMG_SIZE//patch_size
+patch_w  = IMG_SIZE//patch_size
+feat_dim = 384 # vitl14
 
+img_path = "/mnt/g/Code/Dataset/archive/images_001/images/00000001_000.png"
 
 pre_process = transforms.Compose([
-    transforms.Resize(520),
-    transforms.CenterCrop(518), #should be multiple of model patch_size
+    transforms.Resize(IMG_SIZE), #just in case 
+    transforms.CenterCrop(1008), #72 * batch size
     transforms.ToTensor(),
     transforms.Normalize(mean=0.5, std=0.2)
 ])
 
 with torch.no_grad():
     img = Image.open(img_path).convert('RGB')
+    img_transformed = pre_process(img).to(device)
+    features_dict = dinov2_vits14.forward_features(img_transformed.unsqueeze(0))
+    features = features_dict['x_norm_patchtokens']
+    # total_features.append(features)
+
+print(features.shape)
