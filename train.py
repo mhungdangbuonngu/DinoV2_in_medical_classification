ccb8182bfc68226a2fcc09f387847cdefd3e8576
Trong_Minh <trongminhmcw10e@gmail.com>
2024-09-28 00:01:11 +0700

    run this file to train classifier

---
+++ train.py
@@ -0,0 +1,18 @@
+from data_loader import ChestXRDataset
+from torch.utils.data import DataLoader
+import model as md
+import torch
+
+# train_label_path = "/mnt/g/Code/Dataset/archive/train/class_label"
+# train_data_path = "/mnt/g/Code/Dataset/archive/train/img_feature"
+# train_data = ChestXRDataset(train_data_path, train_label_path)
+
+# train_data_loader = DataLoader(train_data, batch_size=32, shuffle=True, num_workers=4)
+
+test_data_path = "/mnt/g/Code/Dataset/archive/test/img_feature"
+test_label_path = "/mnt/g/Code/Dataset/archive/test/class_label"
+test_data = ChestXRDataset(test_data_path, test_label_path)
+
+test_data_loader = DataLoader(test_data, batch_size=32, shuffle=False)
+
+md.train(test_data_loader)
