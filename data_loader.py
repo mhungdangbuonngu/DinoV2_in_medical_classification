bddeec29866a83c5a25a10d78f36620247be6ba3
Trong_Minh <trongminhmcw10e@gmail.com>
2024-09-28 00:00:43 +0700

    add custom dataset for chest xray

---
+++ data_loader.py
@@ -0,0 +1,23 @@
+import torch
+from torch.utils.data import Dataset
+import numpy as np
+import os
+import json
+
+class ChestXRDataset(Dataset):
+    def __init__(self, data_path, label_path): #get all .npy file from directory
+        self.data_files = [os.path.join(data_path, f) for f in os.listdir(data_path) if f.endswith('.npy')]
+        self.label_files = [os.path.join(label_path, f) for f in os.listdir(label_path) if f.endswith('.npy')]
+        with open("label_map.json",'r') as f:
+            self.class_to_idx = json.load(f)
+        self.classes = [key for key in self.class_to_idx]
+
+    def __len__(self):
+        return len(self.data_files)
+
+    def __getitem__(self, idx): #get the data and label based on index
+        data_sample = np.load(self.data_files[idx])
+        label_sample = np.load(self.label_files[idx])
+        
+        #convert to tensors
+        return torch.tensor(data_sample, dtype=torch.float32), torch.tensor(label_sample, dtype=torch.float32)
